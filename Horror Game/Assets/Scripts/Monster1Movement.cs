81eb80b1f4e8b74096188da18a0f286130e291d7
iExploze <54289660+iExploze@users.noreply.github.com>
2023-04-29 20:48:26 -0400

    removed diagonals

--- Horror Game/Assets/Scripts/Monster1Movement.cs
+++ Horror Game/Assets/Scripts/Monster1Movement.cs
@@ -7,26 +7,19 @@ using UnityEngine.Video;
 public class Monster1Movement : MonoBehaviour
 {
     public GameObject player; // Reference to the player GameObject
-    public float chaseSpeed = 1.1f;
-    public float accelerationRate = 0.1f;
-    public float decelerationRate = 0.2f;
-    public float maxSpeedMultiplier = 2f;
-
     public Canvas jumpscareCanvas;
     public VideoPlayer jumpscareVideo;
     public float jumpscareDistanceThreshold = 1f;
+    public float MonsterSpeed = 2;
 
     private Rigidbody2D monsterRb;
     private Animator animator; // Reference to the Animator component
-    private Vector2 lastDirection;
-    private float speedMultiplier = 1f;
 
     void Start()
     {
         monsterRb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>(); // Get the Animator component from the monster
         jumpscareCanvas.GetComponent<CanvasGroup>().alpha = 0; // Hide the jumpscare video at the start
-        jumpscareVideo.loopPointReached += OnJumpscareVideoFinished;
     }
 
     void Update()
@@ -43,26 +36,23 @@ public class Monster1Movement : MonoBehaviour
             Vector2 direction = (Vector2)player.transform.position - (Vector2)transform.position;
             direction.Normalize();
 
-            // Check if the movement direction has changed
-            if (Vector2.Dot(lastDirection, direction) < 0.99f)
+            // Restrict diagonal movement by setting one of the components to zero
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
             {
-                // Decrease the speed multiplier when the direction changes
-                speedMultiplier = Mathf.Max(1f, speedMultiplier - decelerationRate * Time.deltaTime);
+                direction.y = 0;
             }
             else
             {
-                // Increase the speed multiplier while walking in the same direction
-                speedMultiplier = Mathf.Min(maxSpeedMultiplier, speedMultiplier + accelerationRate * Time.deltaTime);
+                direction.x = 0;
             }
 
-            monsterRb.MovePosition(monsterRb.position + direction * chaseSpeed * speedMultiplier * Time.deltaTime);
+
+            monsterRb.MovePosition(monsterRb.position + direction * MonsterSpeed * Time.fixedDeltaTime);
 
             // Update the Animator parameters
             animator.SetFloat("Horizontal", direction.x);
             animator.SetFloat("Vertical", direction.y);
-            animator.SetFloat("Speed", direction.sqrMagnitude);
-
-            lastDirection = direction;
+            animator.SetFloat("Speed", 1);
         }
     }
 
@@ -76,10 +66,5 @@ public class Monster1Movement : MonoBehaviour
             canvasGroup.alpha = 1; // Set the canvas alpha to 1 (fully visible)
             jumpscareVideo.Play();
         }
-    }
-
-    public void OnJumpscareVideoFinished(VideoPlayer vp)
-    {
-        Application.Quit();
     }
 }
