f7a48b28b984415937c8eb203f144cf2a543d420
iExploze <54289660+iExploze@users.noreply.github.com>
2023-04-30 19:45:13 -0400

    finished the menu

---
+++ The_Sancturary/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        // Add code to handle options.
+    }
+
+    public void Credits()
+    {
+        // Add code to handle credits.
+    }
+}
