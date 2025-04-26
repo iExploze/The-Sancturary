6a301c313b56c22da1f94be2ca24b2a91ab97a99
iExploze <54289660+iExploze@users.noreply.github.com>
2025-04-25 23:46:23 -0400

    monster jumpscares

---
+++ The_Sancturary/Assets/Scripts/Monsters/GeoMonsterAI.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class GeoMonsterAI : MonsterBaseAI
+{
+    public Canvas jumpscareCanvas;
+    public VideoPlayer jumpscareVideo;
+    public string deathSceneName = "End Scene";
+
+    protected override void PerformAttack()
+    {
+        agent.isStopped = true;
+
+        if (jumpscareCanvas != null)
+        {
+            jumpscareCanvas.GetComponent<CanvasGroup>().alpha = 1;
+            jumpscareVideo.Play();
+        }
+
+        animator?.SetFloat("Speed", 0f);
+
+        StartCoroutine(WaitForJumpscare());
+    }
+
+    private System.Collections.IEnumerator WaitForJumpscare()
+    {
+        yield return new WaitForSeconds((float)jumpscareVideo.length - 0.3f);
+
+        PlayerPrefs.SetInt("Survived", 0);
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene(deathSceneName);
+    }
+}
