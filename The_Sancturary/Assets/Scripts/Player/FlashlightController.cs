33752ff63b776100aa5788ca721cfa66a61e3715
iExploze <54289660+iExploze@users.noreply.github.com>
2025-04-26 01:15:57 -0400

    added flashlight

---
+++ The_Sancturary/Assets/Scripts/Player/FlashlightController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class FlashlightController : MonoBehaviourPun
+{
+    [SerializeField] private Transform flashlight; // ← manually assign
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            RotateFlashlightToMouse();
+        }
+    }
+
+    void RotateFlashlightToMouse()
+    {
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = mouseWorld - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        flashlight.rotation = Quaternion.Euler(0, 0, angle - 90f);
+    }
+}
