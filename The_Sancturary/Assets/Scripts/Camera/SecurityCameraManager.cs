0039312ae6295bcbf8d08ff3b362e669d87a681c
iExploze <54289660+iExploze@users.noreply.github.com>
2025-05-20 01:35:47 -0400

    small patch up on the door closing

--- The_Sancturary/Assets/Scripts/Camera/SecurityCameraManager.cs
+++ The_Sancturary/Assets/Scripts/Camera/SecurityCameraManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SecurityCameraManager : MonoBehaviour
 {
@@ -9,12 +11,35 @@ public class SecurityCameraManager : MonoBehaviour
     public RenderTexture cameraFeedTexture;
     private int currentCameraIndex = 0;
 
+    public Button closeDoorButton;    // assign your “Close Door” UI Button here
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+
+        // hook up the button click
+        if (closeDoorButton != null)
+            closeDoorButton.onClick.AddListener(OnCloseDoorClicked);
+    }
+
+    private void OnCloseDoorClicked()
+    {
+        // grab the active camera
+        Camera cam = securityCameras[currentCameraIndex];
+        if (cam == null) return;
+
+        // find the door script in its children
+        var door = cam.transform.GetComponentInChildren<DoorController>();
+        if (door != null)
+        {
+            door.ToggleDoor();    // or whatever your door’s “close” method is called
+        }
+        else
+        {
+            Debug.LogWarning($"[SecurityCameraManager] No DoorController found under camera '{cam.name}'");
+        }
     }
 
     private void Start()
